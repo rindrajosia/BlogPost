bc642f2f601692316bd400a40694f2d2dcbf3d15
rindrajosia <rrindrajosia@gmail.com>
2022-02-27 14:13:56 +0300

    Add routes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const ping = require('./routes/ping');
+const posts = require('./routes/posts');
 
 dotenv.config({path: './config/config.env'});
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use('/api/ping', ping);
+
+app.use('/api/posts', posts);
+
 
 app.listen(
   PORT,
